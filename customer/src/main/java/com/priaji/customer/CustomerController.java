b3e00e29fcc1922fff635297bacba9b1bb3ca350
Priaji Oktawibyan Abror <ajipriaji10@gmail.com>
2024-04-25 17:28:12 +0700

    Test tutbah di CustomerController

--- customer/src/main/java/com/priaji/customer/CustomerController.java
+++ customer/src/main/java/com/priaji/customer/CustomerController.java
@@ -17,7 +17,7 @@ public class CustomerController {
 
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRequest customerRegistrationRequest) {
-        log.info("new customer registration Test {}", customerRegistrationRequest);
+        log.info("new customer registration Testing {}", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
     }
 }
